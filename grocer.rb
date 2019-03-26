2e7f90ea13557d5bbe3568a013184d66fa8f2295
Gino Desangles <blushingbox@yahoo.com>
2019-03-26 13:23:32 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,41 @@
 def consolidate_cart(cart)
-  # code here
+  newList = {}
+  
+  cart.each do |itemA|
+    itemA.each do |itemB,valA|
+      if newList[itemB] != nil
+        newList[itemB][:count] += 1
+      else
+        newList[itemB] = valA
+        newList[itemB][:count] = 1
+      end
+    end
+  end
+  
+  return newList
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  
+  newListA = cart
+  
+  coupons.each do |itemA|
+    itemName = itemA[1]
+    
+    if newListA[itemName] != nil &&  newListA[itemName][:count] > coupons[:num]
+      tempVal = newListA[itemName][:count] / coupons[:num]
+      newListA[itemName][:count] -= coupons[:num]
+      tempKey = "#{itemName} W/COUPON"
+      tempItem = {tempKey => {:price => coupons[:cost], :clearence => newListA[itemName][:clearance], :count => tempVal}}
+      
+      if newListA[tempKey] == nil
+        newListA.merge!(tempItem)
+      end       
+    end
+  end
+     
+  return newListA
+  
 end
 
 def apply_clearance(cart)
